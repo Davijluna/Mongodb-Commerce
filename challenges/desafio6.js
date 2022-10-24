77ee5d44bb5c3ff9572ea1452888e97f02c74756
davi <davijluna@gmail.com>
2022-10-24 11:21:06 -0300

    feat: desafio 7

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,4 +1,4 @@
-// 6 - Retorne o nome e as curtidas dos lanches que tiveram curtidas maiores que 10 e menores que 100
+// Retorne o nome e as curtidas dos lanches que tiveram curtidas maiores que 10 e menores que 100
 // Para isso, escreva a query no arquivo desafio6.js
 
 db.produtos.find({ curtidas: { $gt: 10, $lt: 100 } }, { _id: 0, nome: 1, curtidas: 1 }); 
